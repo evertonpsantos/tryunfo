179a0b7e6d712d9911a17287b948428ba19f99d1
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-07-26 13:38:41 -0300

    Requisito 5

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,8 +4,10 @@ import PropTypes from 'prop-types';
 class Form extends React.Component {
   render() {
     const { cardName, cardDescription, cardAttr1, cardAttr2, cardAttr3, cardImage,
-      cardRare, cardTrunfo, hasTrunfo, isSaveButtonDisabled,
+      cardRare, cardTrunfo, isSaveButtonDisabled,
       onInputChange, onSaveButtonClick } = this.props;
+
+    // hasTrunfo,
 
     return (
       <form>
@@ -132,14 +134,14 @@ Form.propTypes = {
   cardImage: PropTypes.string.isRequired,
   cardRare: PropTypes.string.isRequired,
   cardTrunfo: PropTypes.bool.isRequired,
-  hasTrunfo: PropTypes.bool,
+  // hasTrunfo: PropTypes.bool,
   isSaveButtonDisabled: PropTypes.bool.isRequired,
   onInputChange: PropTypes.func.isRequired,
   onSaveButtonClick: PropTypes.func.isRequired,
 };
 
-Form.defaultProps = {
-  hasTrunfo: false,
-};
+// Form.defaultProps = {
+//   hasTrunfo: false,
+// };
 
 export default Form;
