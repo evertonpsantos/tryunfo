863ca10536342c491a01b0132342bc4a3a43e499
evertonpsantos <evertonpsantos8@gmail.com>
2024-03-13 15:53:25 -0300

    adding basic card design

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Card.css';
 
 class Card extends React.Component {
   render() {
@@ -7,24 +8,31 @@ class Card extends React.Component {
       cardImage, cardRare, cardTrunfo, cardType, deleteCard } = this.props;
 
     return (
-      <div className="card">
-        <p data-testid="name-card">{ cardName }</p>
-        <img src={ cardImage } alt={ cardName } data-testid="image-card" />
-        <p data-testid="description-card">{ cardDescription }</p>
-        <p data-testid="attr1-card">{ cardAttr1 }</p>
-        <p data-testid="attr2-card">{ cardAttr2 }</p>
-        <p data-testid="attr3-card">{ cardAttr3 }</p>
-        <p data-testid="rare-card">{ cardRare }</p>
-        { cardTrunfo && <p data-testid="trunfo-card">Super Trunfo</p> }
-        { cardType === 'card' && (
-          <button
-            type="button"
-            data-testid="delete-button"
-            onClick={ deleteCard }
-          >
-            Excluir
-          </button>
-        )}
+      <div className="card-container">
+        <div className="card">
+          <p data-testid="name-card">{ cardName }</p>
+          <img
+            src={ cardImage }
+            alt={ cardName }
+            data-testid="image-card"
+            className="card-img"
+          />
+          <p data-testid="description-card">{ cardDescription }</p>
+          <p data-testid="attr1-card">{ cardAttr1 }</p>
+          <p data-testid="attr2-card">{ cardAttr2 }</p>
+          <p data-testid="attr3-card">{ cardAttr3 }</p>
+          <p data-testid="rare-card">{ cardRare }</p>
+          { cardTrunfo && <p data-testid="trunfo-card">Super Trunfo</p> }
+          { cardType === 'card' && (
+            <button
+              type="button"
+              data-testid="delete-button"
+              onClick={ deleteCard }
+            >
+              Excluir
+            </button>
+          )}
+        </div>
       </div>
     );
   }
